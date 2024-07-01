af625b010468cef2079855046f896406eb361c6a
atrox3d <atrox3d@gmail.com>
2024-07-01 16:25:01 +0200

    chain.from_iterables

--- 07-chain.py
+++ 07-chain.py
@@ -7,9 +7,13 @@ def example_chain():
     print(values)
     assert values == [1, 2, 3, 4, 5, 6, 7, 8]
 
+def get_iterables():
+    return [[1, 2, 3], [4, 5], [6, 7, 8]]
+
 def example_chain_from_iterable():
     # it.chain.from_iterable(iterable_of_iterables)
-    values = list(it.chain.from_iterable([[1, 2, 3], [4, 5], [6, 7, 8]]))
+    iterables = get_iterables()
+    values = list(it.chain.from_iterable(iterables))
     print(values)
     assert values == [1, 2, 3, 4, 5, 6, 7, 8]
 
