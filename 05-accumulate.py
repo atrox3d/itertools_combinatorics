95e87f5b81c95c06754b51b71b7e9c6b83817202
atrox3d <atrox3d@gmail.com>
2024-07-01 09:40:23 +0200

    add function examples

--- 05-accumulate.py
+++ 05-accumulate.py
@@ -1,7 +1,14 @@
 import itertools as it
 
-# it.accumulate(iterable, func=operator.add)
-values = list(it.accumulate([1, 2, 3, 4, 5]))
-# 1+0, 2+1, 3+3, 4+6, 5+10
-print(values)
-assert values == [1, 3, 6, 10, 15]
+def example_accumulate(check:list, iterable:list=None, function=None):
+    iterable = iterable or [1, 2, 3, 4, 5]
+    # it.accumulate(iterable, func=operator.add)
+    values = list(it.accumulate(iterable, func=function))
+    # 1+0, 2+1, 3+3, 4+6, 5+10
+    print(values)
+    assert values == check
+    return values
+
+example_accumulate([1, 3, 6, 10, 15])
+example_accumulate([1, 1, 1, 1, 1], function=min)
+example_accumulate([1, 2, 3, 4, 5], function=max)
