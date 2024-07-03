cb3c506a0e406d8fda58efeff3db9a0721bb7612
atrox3d <atrox3d@gmail.com>
2024-07-03 08:22:21 +0200

    add zip strict example

--- 17-zip_longest.py
+++ 17-zip_longest.py
@@ -8,4 +8,14 @@ def example_zip_longest():
     print(result)
     assert result == [(1, 'a'), (2, 'b'), (3, '')]
 
+def example_zip_strict():
+    data1 = [1, 2, 3]
+    data2 = ['a', 'b']
+    try:
+        result = list(zip(data1, data2, strict=True))
+    except ValueError as ve:
+        print('ERROR', ve)
+
 example_zip_longest()
+
+example_zip_strict()
