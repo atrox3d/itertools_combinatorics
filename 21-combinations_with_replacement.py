90744bfaa17a50843a0ff398b43af2a57605dfe0
atrox3d <atrox3d@gmail.com>
2024-07-04 09:09:27 +0200

    21-combinations_with_replacement.py

---
+++ 21-combinations_with_replacement.py
@@ -0,0 +1,36 @@
+import itertools as it
+
+data = list('abc')
+
+def example_combinations_with_replacement(
+        data, r=None, expected=None, 
+        printer=lambda x:[print(y) for y in x] + [print()]
+    ):
+    # it.combinations_with_replacement(iterable, combo_length)
+    # order doesnt matter: ('a', 'b') == ('b', 'a')
+    # same element is part of the combo: ('a', 'a')
+    print(f'{r=}')
+    result = list(it.combinations_with_replacement(data, r or len(data)))
+    if result:
+        printer(result)
+    else:
+        print(result)
+    if expected is not None:
+        assert result == expected
+    return result
+
+expected = [
+#   ('a', 'b', 'c', 'd')
+#     |  \
+    ('a', 'a'),
+    ('a', 'b'),
+    ('a', 'c'),
+    ('b', 'b'),
+    ('b', 'c'),
+    ('c', 'c'),
+]
+example_combinations_with_replacement(data, r=2, expected=expected)
+
+
+for r in range(len(data)+2):
+    example_combinations_with_replacement(data, r)
