8524229dfc2e0cb2ce62801ec9b68ba4d20cb3d3
atrox3d <atrox3d@gmail.com>
2024-07-04 07:48:07 +0200

    further notes and diagrams

--- 18-product.py
+++ 18-product.py
@@ -6,17 +6,23 @@ expected = [
     (1, 'a'), (1, 'b'), (1, 'c'),
     (2, 'a'), (2, 'b'), (2, 'c'),
 ]
+''' generate product of two iterables
 
-def print_data(*data):
-    for d in data:
-        print(d)
+               1,        2
+            /  |  \   /  |  \ 
+           a,  b, c  a,  b,  c
+  
+           1   1  1  2   2   2
+           a   b  c  a   b   c
+'''
 
 def example_product(data1, data2, expected):
+    ''' generate product of iterables with itertools '''
     # it.product(*iterables, repeat=1)
     # repeat=n means:
     # it.product(*iterables, *iterables, ... [n times])
     #         1a, 1b, 1c, 2a, 2b, 2c
-    print_data(data1, data2)
+    [print(data) for data in (data1, data2)]
     result = list(it.product(data1, data2))
     [print(combo) for combo in result]
     print()
@@ -24,6 +30,7 @@ def example_product(data1, data2, expected):
     return result
 
 def forloop_product(data1, data2):
+    ''' generate product of iterables with for loop '''
     # the same can be done with nested for loop
     # IF we know how many iterables
     product = []
@@ -33,38 +40,43 @@ def forloop_product(data1, data2):
     return product
 
 def comprehension_product(data1, data2):
+    ''' generate product of iterables with comprehension '''
     # and from for loop to comprehension is fairly easy
     return  [(x, y) for x in data1 for y in data2]
 
+''' test reliability of the three methods '''
 product = example_product(data1, data2, expected)
 for_product = forloop_product(data1, data2)
 comp_product = comprehension_product(data1, data2)
-
 assert product == for_product == comp_product == expected
 
-############################################################################
-# compare repeat=2 with multiple copies of iterables
-############################################################################
-'''
-                  1, 2
-                  |
-                  a, b, c
-                /   \ 
-             1,        2
-          /  |  \   /  |  \ 
-         a,  b, c  a,  b,  c
+''' compare repeat=2 with multiple copies of iterables
 
+                   1, 2
+                   |
+                   a, b, c
+                 /   \ 
+               1,        2
+            /  |  \   /  |  \ 
+           a,  b, c  a,  b,  c
+  
+           1   1  1  1   1   1
+           a   a  a  a   a   a
+           1   1  1  2   2   2
+           a   b  c  a   b   c
 '''
-def test_product_repeat(data1, data2, repeat=1):
+def test_product_repeat(data1, data2, repeat=2):
+    ''' test repeat argument of product '''
     result = list(it.product(data1, data2, repeat=repeat))
-    print_data(data1, data2, data1, data2)
+    [print(data) for data in (data1, data2)]
     [print(combo) for combo in result]
     print()
     return result
 
 def test_product_repeat_iterables(data1, data2):
+    ''' simulate repeat argument by passing same iterables two times '''
     result = list(it.product(data1, data2, data1, data2))
-    print_data(data1, data2, data1, data2)
+    [print(data) for data in (data1, data2)]
     [print(combo) for combo in result]
     print()
     return result
